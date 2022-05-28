392b6def17dd145dfa10fabc59e39fa4bfab95bb
Alex Cooper <alex@q2do.com>
2022-05-28 10:25:08 +0100

    Constituency Model

---
+++ src/app/models/constituency.ts
@@ -0,0 +1,11 @@
+export class Constituency {
+    constituencyID = 0;
+    constituency = '';
+    mapItConstituencyID = 0;
+    politicianID = 0;
+    politician = '';
+    politicianImage = '';
+    politicianTwfyUrl = '';
+    politicianTwfyMemberID = 0;
+    politicianWebsite = '';
+}
