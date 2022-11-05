88e0ef2dd1f7695bfa522cbe642f62215e0a2c61
Alex Cooper <alex@free.vote>
2022-11-05 15:14:57 +0000

    PointComments

---
+++ src/app/public/point-comments/point-comments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-point-comments',
+  templateUrl: './point-comments.component.html',
+  styleUrls: ['./point-comments.component.css']
+})
+export class PointCommentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
