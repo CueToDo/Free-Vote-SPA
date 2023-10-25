ffd6b42ddcdf52ff459a9a1a2ae6d8c7cee1443e
Alex Cooper <alex@free.vote>
2023-10-25 16:31:18 +0100

    #150 15.3.21 Group Component - split css classes tabContent and centerStage appData.DayName - changed number parameter to string and use toString on number input (???)

--- src/app/services/app-data.service.ts
+++ src/app/services/app-data.service.ts
@@ -322,21 +322,21 @@ export class AppDataService {
     }
   }
 
-  public DayName(day: number): string {
+  public DayName(day: string): string {
     switch (day) {
-      case 1:
+      case '1':
         return 'Monday';
-      case 2:
+      case '2':
         return 'Tuesday';
-      case 3:
+      case '3':
         return 'Wednesday';
-      case 4:
+      case '4':
         return 'Thursday';
-      case 5:
+      case '5':
         return 'Friday';
-      case 6:
+      case '6':
         return 'Saturday';
-      case 7:
+      case '7':
         return 'Sunday';
       default:
         return '';
