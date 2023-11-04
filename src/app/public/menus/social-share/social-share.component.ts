dd29ce93da204c9337e0beaa87ccc67f7a32bbe1
Alex Cooper <alex@free.vote>
2023-11-04 19:31:45 +0000

    #185 v15.4.6 Social Share updates

--- src/app/public/menus/social-share/social-share.component.ts
+++ src/app/public/menus/social-share/social-share.component.ts
@@ -20,14 +20,29 @@ import { LocalDataService } from 'src/app/services/local-data.service';
   styleUrls: ['./social-share.component.css']
 })
 export class SocialShareComponent implements OnInit {
-  pointTitle = '';
-  slug = '';
-  pointID = 0;
-
+  // used in template
   linkShare = '';
   facebookShare = '';
   twitterShare = '';
   linkToAll = '';
+
+  pointTitle = '';
+  pointHtml = '';
+
+  get pointTitleEncoded(): string {
+    return encodeURIComponent(this.pointTitle);
+  }
+  get pointText(): string {
+    return this.appData.htmlToText(this.pointHtml);
+  }
+
+  get pointTextEncoded(): string {
+    return encodeURIComponent(this.pointText);
+  }
+
+  get linkShareEncoded(): string {
+    return encodeURIComponent(this.linkShare);
+  }
 
   error = '';
 
@@ -43,29 +58,29 @@ export class SocialShareComponent implements OnInit {
 
   DisplayShareLinks(
     pointTitle: string,
-    pointText: string,
+    pointHtml: string,
     slug: string,
     pointID: number
   ): void {
     this.pointTitle = pointTitle;
-    this.slug = slug;
-    this.pointID = pointID;
+    this.pointHtml = pointHtml;
 
+    // slug and pointID allow us to build social link back to free.vote post
     this.linkShare =
-      this.localData.websiteUrlWTS.replace(
-        'https://localhost:7027',
-        'https://free.vote'
-      ) +
+      this.localData.websiteUrlWTS +
       this.localData.PreviousSlashTagSelected +
-      '/' +
-      this.SelectSingleTitle;
+      '/';
+
+    // Add slug or pointID to linkShare
+    if (!!slug) this.linkShare += slug;
+    else this.linkShare += pointID.toString();
 
     const linkShareEncoded = encodeURIComponent(this.linkShare);
 
     this.facebookShare = `https://www.facebook.com/sharer/sharer.php?u=${linkShareEncoded}&amp;src=sdkpreparse`;
-    this.twitterShare = `https://twitter.com/share?ref_src=twsrc%5Etfw&text=${this.appData.htmlToText(
-      pointText
-    )}&url=${linkShareEncoded}`;
+
+    this.twitterShare = `https://twitter.com/share?ref_src=twsrc%5Etfw&text=${this.pointTextEncoded}&url=${linkShareEncoded}`;
+
     this.linkToAll = this.localData.PreviousSlashTagSelected + '/points';
 
     // Client side scripts
@@ -75,18 +90,15 @@ export class SocialShareComponent implements OnInit {
     }
   }
 
-  // PointTitle or PointID to be able to select single point
-  get SelectSingleTitle(): string {
-    if (!!this.slug) {
-      return this.slug;
-    } else {
-      return this.pointID.toString();
-    }
-  }
-
   ShareByEmail() {
     window.open(
-      `mailto:?subject=${this.pointTitle}&body=Hi,%0D%0A%0D%0ATake a look at this from the ${this.localData.website} website - what do you think?%0D%0A%0D%0A${this.linkShare}`,
+      `mailto:?subject=${this.pointTitle}&body=Hi,
+%0D%0A%0D%0A
+Take a look at this from the ${this.localData.website} website - what do you think?
+%0D%0A%0D%0A
+${this.pointText}
+%0D%0A%0D%0A
+${this.linkShare}`,
       '_blank'
     );
   }
