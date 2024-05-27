63c4885e8c7d9568a80e8090114e623eb6762711
Alex Cooper <alex@free.vote>
2024-05-27 09:28:34 +0100

    restore original

--- server.ts
+++ server.ts
@@ -7,27 +7,13 @@ import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 
 import bootstrap from './src/main.server';
+import { existsSync } from 'node:fs';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
 
-  // set distFolder from current working directory
-  const workingDirectory = process.cwd();
-  const browserDistFolder = resolve(
-    workingDirectory,
-    '../../../dist/free-vote/browser'
-  );
-  const indexHtml = join(browserDistFolder, 'index.html');
-
-  console.log(
-    `workingDirectory:${workingDirectory} BrowserDistFolder:${browserDistFolder} indexHtml:${indexHtml}`
-  );
-
-  const commonEngine = new CommonEngine();
-
-  // ---- REDIRECT TO HTTPS ---- //
-
+  // 1. REDIRECT TO HTTPS
   server.use(function (req, res, next) {
     // if (!req.secure) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
@@ -36,35 +22,79 @@ export function app(): express.Express {
     next();
   });
 
-  server.set('view engine', 'html');
-  server.set('views', browserDistFolder);
+  // 2. Original
+  const distFolder = join(process.cwd(), 'dist/free-vote/browser');
+  const indexHtml = existsSync(join(distFolder, 'index.original.html'))
+    ? 'index.original.html'
+    : 'index';
 
-  // Example Express Rest API endpoints
+  // 2. Set distFolder from current working directory
+  // const workingDirectory = process.cwd();
+  // const browserDistFolder = resolve(
+  //   workingDirectory,
+  //   '../../../dist/free-vote/browser'
+  // );
+  // const indexHtml = join(browserDistFolder, 'index.html');
+
+  // console.log(
+  //   `workingDirectory:${workingDirectory} BrowserDistFolder:${browserDistFolder} indexHtml:${indexHtml}`
+  // );
+
+  //3. Original
+  server.set('view engine', 'html');
+  server.set('views', distFolder);
+
+  // 3. server.set
+  // server.set('view engine', 'html');
+  // server.set('views', browserDistFolder);
+
+  // 4. Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
+
+  //4a. Original
   server.get(
     '*.*',
-    express.static(browserDistFolder, {
+    express.static(distFolder, {
       maxAge: '1y'
     })
   );
 
-  // All regular routes use the Angular engine
-  server.get('*', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
+  // 4a. Serve static files from /browser
+  // server.get(
+  //   '*.*',
+  //   express.static(browserDistFolder, {
+  //     maxAge: '1y'
+  //   })
+  // );
 
-    commonEngine
-      .render({
-        bootstrap,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }]
-      })
-      .then(html => res.send(html))
-      .catch(err => next(err));
+  // 4b. Original
+  // All regular routes use the Universal engine
+  server.get('*', (req, res) => {
+    res.render(indexHtml, {
+      req,
+      providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }]
+    });
   });
 
+  // 4b. All regular routes use the Angular engine
+  //   const commonEngine = new CommonEngine();
+
+  //   server.get('*', (req, res, next) => {
+  //     const { protocol, originalUrl, baseUrl, headers } = req;
+
+  //     commonEngine
+  //       .render({
+  //         bootstrap,
+  //         documentFilePath: indexHtml,
+  //         url: `${protocol}://${headers.host}${originalUrl}`,
+  //         publicPath: browserDistFolder,
+  //         providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }]
+  //       })
+  //       .then(html => res.send(html))
+  //       .catch(err => next(err));
+  //   });
+
+  // end function app()
   return server;
 }
 
