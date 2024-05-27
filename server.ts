14b1f85339779d52f44c580bce389d3ba346d40f
Alex Cooper <alex@free.vote>
2024-05-27 09:52:10 +0100

    Back to updated CommonEngine

--- server.ts
+++ server.ts
@@ -23,76 +23,77 @@ export function app(): express.Express {
   });
 
   // 2. Original
-  const distFolder = join(process.cwd(), 'dist/free-vote/browser');
-  const indexHtml = existsSync(join(distFolder, 'index.original.html'))
-    ? 'index.original.html'
-    : 'index';
+  // const distFolder = join(process.cwd(), 'dist/free-vote/browser');
+  // const indexHtml = existsSync(join(distFolder, 'index.original.html'))
+  //   ? 'index.original.html'
+  //   : 'index';
 
   // 2. Set distFolder from current working directory
-  // const workingDirectory = process.cwd();
-  // const browserDistFolder = resolve(
-  //   workingDirectory,
-  //   '../../../dist/free-vote/browser'
-  // );
-  // const indexHtml = join(browserDistFolder, 'index.html');
+  const workingDirectory = process.cwd();
+  const browserDistFolder = resolve(
+    workingDirectory,
+    '../../../dist/free-vote/browser'
+  );
+  const indexHtml = join(browserDistFolder, 'index.html');
 
-  // console.log(
-  //   `workingDirectory:${workingDirectory} BrowserDistFolder:${browserDistFolder} indexHtml:${indexHtml}`
-  // );
+  console.log(
+    `workingDirectory:${workingDirectory} BrowserDistFolder:${browserDistFolder} indexHtml:${indexHtml}`
+  );
 
   //3. Original
-  server.set('view engine', 'html');
-  server.set('views', distFolder);
+  // server.set('view engine', 'html');
+  // server.set('views', distFolder);
 
   // 3. server.set
-  // server.set('view engine', 'html');
-  // server.set('views', browserDistFolder);
+  server.set('view engine', 'html');
+  server.set('views', browserDistFolder);
 
   // 4. Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
 
   //4a. Original
-  server.get(
-    '*.*',
-    express.static(distFolder, {
-      maxAge: '1y'
-    })
-  );
-
-  // 4a. Serve static files from /browser
   // server.get(
   //   '*.*',
-  //   express.static(browserDistFolder, {
+  //   express.static(distFolder, {
   //     maxAge: '1y'
   //   })
   // );
 
+  // 4a. Serve static files from /browser
+  server.get(
+    '*.*',
+    express.static(browserDistFolder, {
+      maxAge: '1y'
+    })
+  );
+
   // 4b. Original
   // All regular routes use the Universal engine
-  server.get('*', (req, res) => {
-    res.render(indexHtml, {
-      req,
-      providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }]
-    });
-  });
+  // server.get('*', (req, res) => {
+  //   res.render(indexHtml, {
+  //     req,
+  //     providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }]
+  //   });
+  // });
 
   // 4b. All regular routes use the Angular engine
-  //   const commonEngine = new CommonEngine();
 
-  //   server.get('*', (req, res, next) => {
-  //     const { protocol, originalUrl, baseUrl, headers } = req;
+  const commonEngine = new CommonEngine();
 
-  //     commonEngine
-  //       .render({
-  //         bootstrap,
-  //         documentFilePath: indexHtml,
-  //         url: `${protocol}://${headers.host}${originalUrl}`,
-  //         publicPath: browserDistFolder,
-  //         providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }]
-  //       })
-  //       .then(html => res.send(html))
-  //       .catch(err => next(err));
-  //   });
+  server.get('*', (req, res, next) => {
+    const { protocol, originalUrl, baseUrl, headers } = req;
+
+    commonEngine
+      .render({
+        bootstrap,
+        documentFilePath: indexHtml,
+        url: `${protocol}://${headers.host}${originalUrl}`,
+        publicPath: browserDistFolder,
+        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }]
+      })
+      .then(html => res.send(html))
+      .catch(err => next(err));
+  });
 
   // end function app()
   return server;
