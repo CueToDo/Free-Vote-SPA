512a0099fb155c7e57e43366fbcf0a1b732f4f50
Alex Cooper <alex@free.vote>
2024-06-09 15:54:47 +0100

    angular.json outputPath - dist only not subdirectory ssr server.ts server from dist/browser

--- server.ts
+++ server.ts
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/free-vote/browser'));
+app.use(express.static(__dirname + '/dist/browser'));
 
 app.get('/*', function (_req: any, res: { sendFile: (arg0: any) => void }) {
-  res.sendFile(path.join(__dirname + '/dist/free-vote/browser/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/browser/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
